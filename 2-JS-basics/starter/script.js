1384d67696f7dbeadc9b79bae18d9788f8db3152
crabjuice <vojirasek@gmail.com>
2019-05-12 14:24:12 +0200

    arrays

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -317,11 +317,101 @@ switch (true){
         console.log('It is split between Mike and John ' + averageMike);
         break;
 }
+*/
+
+/**********************
+ * Functions
+ 
+
+function calculateAge(birthYear) {
+    return 2018 - birthYear;
+}
+
+var ageJohn = calculateAge(1990);
+var ageMike = calculateAge(1948);
+var ageJane = calculateAge(1969);
+console.log(ageJohn, ageMike, ageJane);
+
+
+function yearsUnstilRetirement(year, firstName) {
+    var age = calculateAge(year);
+    var retirement = 65 - age;
+
+    if (retirement > 0){
+    console.log(firstName + ' retires in ' + retirement + ' years.');
+} else {
+    console.log(firstName + ' is already retired.')
+}
+}
+
+yearsUnstilRetirement(1990, 'John');
+yearsUnstilRetirement(1948, 'Mike');
+yearsUnstilRetirement(1969, 'Jane');
 
 */
 
+/*************************
+ * Function Statements and Expressions
+ 
+
+// Fuction declaration
+// function whatDoYouDo(job, firstName){}
 
 
+ // Function expression
+var whatDoYouDo = function(job, firstName) {
+    switch(job) {
+        case 'teacher':
+            return firstName + ' teaches kids how to code.';
+        case 'driver':
+            return firstName + ' drives a cab in Lisbon.'
+        case 'designer':
+            return firstName + ' designs beautiful websites.';
+        default:
+            return firstName + ' does something else.';
+    }
+
+}
+
+console.log(whatDoYouDo('teacher', 'John'));
+console.log(whatDoYouDo('designer', 'Jane'));
+console.log(whatDoYouDo('retired', 'Mark'));
+
+*/
+
+/*****************
+ * Arrays
+ 
+
+ // Initialize new array
+var names = ['John', 'Mark', 'Jane'];
+var years = new Array(1990, 1969, 1948);
+
+console.log(names[0]);
+console.log(names);
+console.log(names.length);
+
+// Mutate array data
+names[1] = 'Ben';
+names[names.length] = 'Mary';
+console.log(names);
+
+// Different data types
+var john = ['John', 'Smith', 1990, 'teacher', false];
+
+john.push('blue'); //nakonec prida
+john.unshift('Mr.'); // nazacatek prida
+console.log(john);
+
+john.pop();      //delete konec
+john.shift(); // delete zacatek
+console.log(john);
+
+console.log(john.indexOf(1990));
+
+var isDesigner = john.indexOf('designer') === -1 ? 'John is NOT a designer' : 'John IS a designer';
+console.log(isDesigner);
+*/
 
 
 

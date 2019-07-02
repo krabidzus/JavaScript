f19c73094ccc1243b963add764a232e263ba36be
crabjuice <vojirasek@gmail.com>
2019-07-02 20:50:00 +0200

    kostky pokracovani

--- 4-DOM-pig-game/starter/app.js
+++ 4-DOM-pig-game/starter/app.js
@@ -34,10 +34,56 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
 
 
     // 3. Update the round score If the rolled number was NOT a 1
-
-
-
+    if (dice !== 1) { //vsechno krome 1
+        //Add score
+        roundScore += dice;
+        //roundScore = roundScore + dice
+        document.querySelector('#current-' + activePlayer).textContent = roundScore;
+    } else {
+       nextPlayer();
+    }
 });
+
+document.querySelector('.btn-hold').addEventListener('click', function(){
+    // Add Current score to global score
+    scores[activePlayer] += roundScore;
+    //scores[activePlayer] = scores[activePlayer] + roundScore;
+
+    // Update the UI
+    document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+
+    // check if player won the game
+
+    if (scores[activePlayer] >= 20){
+        document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
+        document.querySelector('.dice').style.display = 'none';
+        document.querySelector('.player-' + activePlayer + '-panel'.classList.add('winner');
+        document.querySelector('.player-' + activePlayer + '-panel'.classList.remove('active');
+    } else {
+        // next player
+        nextPlayer();
+    }
+});
+
+
+
+function nextPlayer() {
+//Next player
+activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+roundScore = 0;
+
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+document.querySelector('.player-0-panel').classList.toggle('active');
+document.querySelector('.player-1-panel').classList.toggle('active');
+
+
+//document.querySelector('.player-0-panel').classList.remove('active');
+//document.querySelector('.player-1-panel').classList.add('active');
+
+document.querySelector('.dice').style.display = 'none';
+}
 
 
 //document.querySelector('#current-' + activePlayer).textContent = dice;
